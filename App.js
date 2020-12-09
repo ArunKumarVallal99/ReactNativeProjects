b52c1c06419c25a54b49d950dbcfad6e4e6ca973
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-09 18:44:24 +0530

    State and Props

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';  
+import {StyleSheet,Text,View,TextInput,TouchableOpacity} from 'react-native';  
+  
+export default class App extends Component {  
+    
+      state = {  
+            password: '',  
+            isPasswordVisible: true,  
+            toggleText: 'Show',  
+        };  
+     
+    handled = () => {  
+        const { isPasswordVisible } = this.state;  
+        if (isPasswordVisible) {  
+            this.setState({ isPasswordVisible: false });  
+            this.setState({ toggleText: 'Hide' });  
+        } else {  
+            this.setState({ isPasswordVisible: true });  
+            this.setState({ toggleText: 'Show' });  
+        }  
+    };  
+    render() {  
+        return (  
+            <View style={styles.container}>  
+                <TextInput  
+                    secureTextEntry={this.state.isPasswordVisible}  
+                    style={styles.textInputStyle}  
+                />  
+                <TouchableOpacity onPress={this.handled}>  
+                    <Text  style={{fontSize: 20}}>{this.state.toggleText}</Text>  
+                </TouchableOpacity>  
+            </View>  
+        );  
+    }  
+}  
+const styles = StyleSheet.create({  
+    container: {  
+      flex: 1,  
+      justifyContent: 'center',  
+      alignItems: 'center',  
+    },
+    textInputStyle:{
+      width: 400, 
+      height: 50,
+      backgroundColor: '#a7a6a9', 
+      color: 'white',
+      fontSize: 20 
+    },
+});
